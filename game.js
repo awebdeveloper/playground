f868140724a7b1c01a488cdac5c2d8598148df19
ui <iprathik@gmail.com>
2014-02-19 14:17:20 +0530

    linting and code cleanup

--- game.js
+++ game.js
@@ -1,21 +1,24 @@
 /*jslint browser: true*/
+/*global getRandomInt, getSupportedStyleName, createNode */
 
-    var racetrack        = document.getElementById('racetrack');
-    var raceresult       = document.getElementById('raceresult');
-    var resultDiv     = document.getElementById('winnerresult');
+(function () {
+    "use strict";
+    var racetrack   = document.getElementById('racetrack'),
+        raceresult  = document.getElementById('raceresult'),
+        resultDiv   = document.getElementById('winnerresult');
 
     function Players(ele, game) {
+
         this.movePositions  = [0, 40, 80, 120];
-        this.moveBy         = 5;
         this.el             = ele;
-        this.i              = 0;
+        this.stepsMoved     = 0;
         this.stop           = 1;
         this.position       = 0;
-        this.timeout        = 120 + getRandomInt(1, (game.noOfPlayers * 10));
+        this.timeout        = 120;
 
 
         this.animate = function () {
-            /* Stop if stopped */
+            /* Stop if stopped or exceding boundry*/
             var playerPosition = this.el.getBoundingClientRect();
             if (this.stop || playerPosition.left > (racetrack.offsetWidth - 30)) {
                 this.el.style.backgroundPosition = '120px 0px';
@@ -23,22 +26,22 @@
                 return;
             }
 
-            /* Prepare Next Move */        
-            setTimeout(function (_this) {
-                if (_this.i < _this.movePositions.length) {
-                    _this.i++;
+            /* Prepare Next Move */
+            setTimeout(function (that) {
+                if (that.stepsMoved < that.movePositions.length) {
+                    that.stepsMoved += 1;
                 } else {
-                    _this.i = 0;
-                   _this.timeout = 120 + getRandomInt(1, (game.noOfPlayers * 10));
+                    that.stepsMoved = 0;
+                    that.timeout = 120 + getRandomInt(1, 30);
                 }
-                _this.move();
-                _this.animate();
+                that.move();
+                that.animate();
             }, this.timeout, this);
 
         };
 
         this.restart = function () {
-            this.i = 0;
+            this.stepsMoved = 0;
             this.stop = 1;
             this.move(0, 0);
         };
@@ -55,11 +58,11 @@
         };
 
         this.move = function (to, positionIndex) {
-            positionIndex   = positionIndex || this.i;
-            this.position   = to || (this.position + this.moveBy);
+            positionIndex   = positionIndex || this.stepsMoved;
+            this.position   = to || (this.position + 5);
 
             this.el.style.backgroundPosition = '-' + this.movePositions[positionIndex] + 'px 0px';
-            this.el.style[getSupportedPropertyName('transform')] = 'translate(' +  this.position + 'px)';
+            this.el.style[getSupportedStyleName('transform')] = 'translate(' +  this.position + 'px)';
         };
     }
 
@@ -73,64 +76,55 @@
         this.chosen         =  1;
         this.players        = [];
         this.winningPlayer  = '';
-        
-        
-        this.init_game = function(){
-            var _this = this;
-            
-            document.getElementById('selectplayers').addEventListener('change', function(){
-                if(el.options[el.selectedIndex].value !== ''){
-                    el.nextElementSibling.removeAttribute('disabled');
-                }
-                else{
-                    el.nextElementSibling.setAttribute("disabled","disabled");
-                }
-            });
-            
-            document.getElementById('noofplayers').addEventListener('change', function(){
+
+
+        this.init_game = function () {
+            var that = this;
+
+            document.getElementById('noofplayers').addEventListener('change', function () {
                 var optionsHtml = '',
                     i           = 0;
-                
-                if(this.value < 3){
+
+                if (this.value < 3) {
                     this.value = 3;
                 }
-                
-                for(i=1; i<=this.value; i++){
-                    optionsHtml = optionsHtml + '<option value="'+ i +'">'+ i +'</option>';
+
+                for (i = 1; i <= this.value; i += 1) {
+                    optionsHtml = optionsHtml + '<option value="' + i + '">' + i + '</option>';
                 }
-                
+
                 document.getElementById('selectplayers').innerHTML = optionsHtml;
             });
 
-            document.getElementById('startgame').addEventListener('click',function(){
-                document.getElementById('racetrack').style.display = 'block'; 
+            document.getElementById('startgame').addEventListener('click', function () {
+                document.getElementById('racetrack').style.display = 'block';
                 document.getElementById('intro').style.display = 'none';
-                _this.noOfPlayers    =  parseInt(document.getElementById('noofplayers').value, 10);
-                _this.chosen         =  parseInt(document.getElementById('selectplayers').value, 10);
-                
-                for (i = 1; i <= _this.noOfPlayers; i++) {
+                that.noOfPlayers    =  parseInt(document.getElementById('noofplayers').value, 10);
+                that.chosen         =  parseInt(document.getElementById('selectplayers').value, 10);
+
+                for (i = 1; i <= that.noOfPlayers; i += 1) {
                     racetrack.appendChild(createNode(track_tmpl.replace('{{ x }}', i)));
-                    _this.players.push(new Players(document.getElementById('player' + i), _this));
+                    that.players.push(new Players(document.getElementById('player' + i), that));
                 }
-                
-                _this.play();
+
+                that.play();
             });
         };
 
         this.pause = function () {
-            for (i = 0; i < this.noOfPlayers; i++) {
+            for (i = 0; i < this.noOfPlayers; i += 1) {
                 this.players[i].pause();
             }
         };
 
         this.restart = function () {
-            for (i = 0; i < this.noOfPlayers; i++) {
+            for (i = 0; i < this.noOfPlayers; i += 1) {
                 this.players[i].restart();
             }
         };
 
         this.play = function () {
-            for (i = 0; i < this.noOfPlayers; i++) {
+            for (i = 0; i < this.noOfPlayers; i += 1) {
                 this.players[i].play();
             }
         };
@@ -139,8 +133,7 @@
             if (this.winningPlayer === '') {
                 this.winningPlayer = ele.id;
             }
-            finishedPlayers++;
-            console.log(this.noOfPlayers);
+            finishedPlayers += 1;
             if (finishedPlayers === this.noOfPlayers) {
                 racetrack.style.display  = 'none';
                 raceresult.style.display = 'block';
@@ -152,3 +145,6 @@
             }
         };
     }
+    
+    window.Game = Game;
+}());
--- util.js
+++ util.js
@@ -1,4 +1,6 @@
+/*jslint browser: true*/
 function createNode(htmlStr) {
+    "use strict";
 	var frag = document.createDocumentFragment(),
 		temp = document.createElement('div');
 	temp.innerHTML = htmlStr;
@@ -9,22 +11,59 @@ function createNode(htmlStr) {
 }
 
 function getRandomInt(min, max) {
+    "use strict";
 	var result = Math.floor(Math.random() * (max - min + 1)) + min;
 	return result;
 }
 
-function getSupportedPropertyName(property) {
-	var prefix = ['moz','o','webkit','ms','khtml'];
+function getSupportedStyleName(property) {
+    "use strict";
+	var prefix = ['moz', 'o', 'webkit', 'ms', 'khtml'],
+        i,
+        propertyName;
 
-	if (typeof document.body.style[property] != "undefined") {
+	if (typeof document.body.style[property] !== "undefined") {
 		return property;
 	}
 
-	for (var i = 0; i < prefix.length; i++) {
-		var propertyName = prefix[i] + property.charAt(0).toUpperCase() + property.slice(1);
-		if (typeof document.body.style[propertyName] != "undefined") {
+	for (i = 0; i < prefix.length; i += 1) {
+		propertyName = prefix[i] + property.charAt(0).toUpperCase() + property.slice(1);
+		if (typeof document.body.style[propertyName] !== "undefined") {
 			return propertyName;
 		}
 	}
 	return null;
 }
+
+(function () {
+    "use strict";
+    var lastTime    = 0,
+        prefix     = ['moz', 'o', 'webkit', 'ms', 'khtml'],
+        i;
+    for (i = 0; i < prefix.length && !window.requestAnimationFrame; i += 1) {
+        window.requestAnimationFrame = window[prefix[i] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame  = window[prefix[i] + 'CancelAnimationFrame']
+                                    || window[prefix[i] + 'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function (callback, element) {
+            var currTime    = new Date().getTime(),
+                timeToCall  = Math.max(0, 16 - (currTime - lastTime)),
+                id;
+            
+            id = window.setTimeout(function () {
+                callback(currTime + timeToCall);
+            }, timeToCall);
+            
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+ 
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
+    }
+}());
